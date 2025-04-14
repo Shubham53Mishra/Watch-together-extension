4eb911b7619f34c0e7e106f83c2dd5019232c5b3
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-04-14 14:50:40 +0530

    set the path

--- socket.js
+++ socket.js
@@ -1,17 +1,23 @@
-window.socket = io("https://watch-together-backend-2z6t.onrender.com");
+// Add Socket.IO CDN dynamically
+const script = document.createElement("script");
+script.src = "https://cdn.socket.io/4.5.4/socket.io.min.js";
+script.onload = () => {
+  window.socket = io("https://watch-together-backend-2z6t.onrender.com");
 
-window.socket.on("play", ({ time }) => {
-  const video = document.querySelector("video");
-  if (video) {
-    video.currentTime = time;
-    video.play();
-  }
-});
+  window.socket.on("play", ({ time }) => {
+    const video = document.querySelector("video");
+    if (video) {
+      video.currentTime = time;
+      video.play();
+    }
+  });
 
-window.socket.on("pause", ({ time }) => {
-  const video = document.querySelector("video");
-  if (video) {
-    video.currentTime = time;
-    video.pause();
-  }
-});
+  window.socket.on("pause", ({ time }) => {
+    const video = document.querySelector("video");
+    if (video) {
+      video.currentTime = time;
+      video.pause();
+    }
+  });
+};
+document.head.appendChild(script);
